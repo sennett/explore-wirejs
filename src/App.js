46aa159b48b0ee3023619628dccd43953cf673d9
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-25 16:23:56 +0100

    this commit reproduces issue

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 define(['box2dweb'], function (box2dweb) {
-//define(function () {
-	var App = function(moduleA, moduleB, staticData){
-		this.moduleA = moduleA;
-		this.moduleB = moduleB;
-		this.staticData = staticData;
+	var App = function(){
 	};
 
 	App.prototype = {
 		run: function(){
-			this.moduleA.doSomethingModuleA();
-			this.moduleA.applyStaticData(this.staticData);
-			this.moduleB.setModuleAAtRuntime(this.moduleA);
-			this.moduleB.pleaseDoSomethingModuleB();
+			console.log(box2dweb);
 		}
 	};
 
