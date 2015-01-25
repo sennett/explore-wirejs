0bf6224791c428715e088ad8cd0f7fdf96f64757
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-25 18:02:42 +0100

    builds without errors.  does not run though

---
+++ build.js
@@ -0,0 +1,11 @@
+({
+	mainConfigFile: 'src/main.js',
+	name: "main",
+	out: "build/main-built.js",
+	paths: {
+		'wire/builder/rjs': '../node_modules/wire-rjs-builder/builder',
+		'src': '../src'
+	},
+	optimize: "none",
+	findNestedDependencies: false
+})
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 require.config({
 	baseUrl: ".",
 	packages: [
-		{ name: 'wire', location: '/bower_components/wire', main: 'wire' },
-		{ name: 'when', location: '/bower_components/when', main: 'when' },
-		{ name: 'meld', location: '/bower_components/meld', main: 'meld' },
-		{ name: 'poly', location: '/bower_components/poly', main: 'poly' }
+		{ name: 'wire', location: '../bower_components/wire', main: 'wire' },
+		{ name: 'when', location: '../bower_components/when', main: 'when' },
+		{ name: 'meld', location: '../bower_components/meld', main: 'meld' },
+		{ name: 'poly', location: '../bower_components/poly', main: 'poly' }
 	],
 	paths:{
-		box2dweb: '/vendor/Box2dWeb-2.1.a.3'
+		box2dweb: '../vendor/Box2dWeb-2.1.a.3'
 	},
 	shim: {
 		box2dweb: {
