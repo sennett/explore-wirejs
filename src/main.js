e3c0c3d4d0d75827b638130d92e2bd254a49c6bb
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-25 17:13:14 +0100

    move to requirejs

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
-(function(curl) {
-
-	var config = {
-		packages: [
-			{ name: 'curl', location: 'bower_components/curl/src/curl', main: 'curl' },
-			{ name: 'wire', location: 'bower_components/wire', main: 'wire' },
-			{ name: 'when', location: 'bower_components/when', main: 'when' },
-			{ name: 'meld', location: 'bower_components/meld', main: 'meld' },
-			{ name: 'poly', location: 'bower_components/poly', main: 'poly' }
-		],
-		paths: {
-			box2dweb: {
-				location: 'vendor/Box2dWeb-2.1.a.3.js',
-				config: { loader: 'curl/loader/legacy', exports: 'Box2D' }
-			}
+require.config({
+	baseUrl: ".",
+	packages: [
+		{ name: 'wire', location: '/bower_components/wire', main: 'wire' },
+		{ name: 'when', location: '/bower_components/when', main: 'when' },
+		{ name: 'meld', location: '/bower_components/meld', main: 'meld' },
+		{ name: 'poly', location: '/bower_components/poly', main: 'poly' }
+	],
+	paths:{
+		box2dweb: '/vendor/Box2dWeb-2.1.a.3'
+	},
+	shim: {
+		box2dweb: {
+			exports: 'Box2D'
 		}
-	};
+	}
+});
 
-	curl(config, ['wire!src/wireSpec']);
-
-})(curl);
+require(['wire!src/wireSpec']);
