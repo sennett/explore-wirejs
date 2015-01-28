0f2925142e0ee020b7edcd969ea262d146f74e30
Anthony Sennett <anthony.sennett@gmail.com>
2015-01-28 20:50:50 +0100

    removing poly (not used)

--- src/amdBaseConfig.js
+++ src/amdBaseConfig.js
@@ -3,8 +3,7 @@ require.config({
 	packages: [
 		{ name: 'wire', location: '../bower_components/wire', main: 'wire' },
 		{ name: 'when', location: '../bower_components/when', main: 'when' },
-		{ name: 'meld', location: '../bower_components/meld', main: 'meld' },
-		{ name: 'poly', location: '../bower_components/poly', main: 'poly' }
+		{ name: 'meld', location: '../bower_components/meld', main: 'meld' }
 	],
 	paths:{
 		box2dweb: '../vendor/Box2dWeb-2.1.a.3'
